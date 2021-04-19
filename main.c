f7e4346ea0e392ae6e81e3a838dbd2892c84a71e
mahmoud <mahmoud.mohammed1611@gmail.com>
2021-04-19 16:33:26 +0200

    student_1

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+struct students { char student_name[20];
+    int student_id ;
+    int year; int month; int day;
+    int student_score;};
+/*
+ * from line 7 to line 200 linkd list function
+ * from line 201 to 400 dynamic array function
+ */
+int main() {
+    printf("welcome to our code \n");
+    return 0;}
+
